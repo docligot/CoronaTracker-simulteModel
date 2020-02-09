c0591687be3b6f7b9c7f0bcc3c69b74f45cb52d6
YiranJing <yjin5856@uni.sydney.edu.au>
2020-02-09 23:11:48 +1100

    add suspect column, records since 2019-12-01

--- data_processing/DXY_AreaData_query.py
+++ data_processing/DXY_AreaData_query.py
@@ -11,6 +11,7 @@ resource: https://github.com/jianxu305/nCov2019_analysis
 """
 
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 import utils   # some convenient functions
 import pandas as pd
@@ -22,45 +23,53 @@ from googletrans import Translator # package used to translate Chinese into Engl
 
 translator = Translator()
 ## Resoruce for Chinese - English Translation
+## Resoruce for Chinese - English Translation
 with open('chineseProvince_to_EN.pkl','rb') as f:
     prov_dict = pkl.load(f)
 with open('chineseCity_to_EN.pkl','rb') as f:
-    city_dict = pkl.load(f)
-    # some new area is not included in city_dict, so manually update
-    city_dict['密云区'] = 'MiYun District' 
-    city_dict['武清区'] = 'WuQing District'
-    city_dict['兵团第十二师'] = '12-th BingTuanGroup'
-    city_dict['昌吉州'] = 'ChangJi'
-    city_dict['阿克苏地区'] = 'AKeSu'
+    city_dict = pkl.load(f)    
     
 
-def getProvinceTranslation(name):
-    return prov_dict[name]
+def isNaN(num):
+    return num != num
 
 
-def getCityTranslation(name):
-    try:
-        return city_dict[name]
-    except KeyError:
-        print(name + 'cannot be translated, ask Yiran to mannully Translate\n')
-
+        
 def translate_to_English(data, prov_dict, city_dict):
     """Translate Chinese in dataset to English
     """        
-    data['provinceName'] = data['provinceName'].apply(getProvinceTranslation)
-    data['cityName'] = data['cityName'].apply(getCityTranslation)
+    data['province'] = data['province'].apply(getProvinceTranslation)
+    data['city'] = data['city'].apply(getCityTranslation)
     return data
+    
+def getProvinceTranslation(name):
+    if not isNaN(name):
+        return prov_dict[name]
+    else: 
+        return name
+
+def getCityTranslation(name):
+    try:
+        if not isNaN(name) :
+            return city_dict[name]
+        else:
+            return name
+    except KeyError:
+        if name != None and len(name)>0:
+            print(name + 'cannot be translated, ask Yiran to mannully Translate\n')
         
         
 def main():
     
-    DXYArea = utils.load_chinese_data() # Query latest Regional Data from DXY
-    daily_frm_DXYArea = utils.aggDaily(DXYArea)  # aggregate to daily data (keep the latest records every day)
+    ## Query the latest data
+    os.system('python dataset.py')
     
-    ## Translate into English
-    daily_frm_DXYArea = translate_to_English(daily_frm_DXYArea, prov_dict, city_dict)
+    DXYArea = pd.read_csv('../data/DXYArea.csv')
+    # select column
+    DXYArea = DXYArea[['date','country','countryCode','province', 'city', 'confirmed', 'suspected', 'cured', 'dead']]
+    # clean data 
+    DXYArea["city"] = DXYArea["city"].str.split('市',expand=True)[1].str[:-1]
     
-    daily_frm_DXYArea.to_csv ('../data/DXYArea.csv', index = None, header=True)
     print("Save area daily dataset (English) into ../data/DXYArea.csv")
     
 if __name__ == '__main__':
