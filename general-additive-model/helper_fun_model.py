07d44c342909691d28fef5c352f89a15e73a5105
leebond <leebond_guanhua@hotmail.com>
2020-02-11 01:35:43 +0800

    update GAM model

--- general-additive-model/helper_fun_model.py
+++ general-additive-model/helper_fun_model.py
@@ -11,11 +11,26 @@ import pandas as pd
 import pandas
 import datetime
 import matplotlib.dates as mdates
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import warnings
 warnings.filterwarnings('ignore')
+
+########################
+## dataset help function
+########################
+
+def get_province_df(df, provinceName: str) -> pandas.core.frame.DataFrame:
+    """
+    Return time series data of given province
+    """
+    return df[(df['province']==provinceName) & (df['city'].isnull())]
+
+
+def get_China_total(df) -> pandas.core.frame.DataFrame:
+    """
+    Return time series data of China total (including HK and Taiwan)
+    """
+    return df[(df['countryCode']=='CN') & (df['province'].isnull())]
 
 ##################
 ## Clean data
@@ -24,38 +39,61 @@ def add_days(DXYArea: pandas.core.frame.DataFrame) -> pandas.core.frame.DataFram
     """
     Create a new column: Days, number of days after 2019-12-08 (detect the first case)
     """
-    DXYArea['updateDate'] = pd.to_datetime(DXYArea['updateDate'])
+    DXYArea['date'] = pd.to_datetime(DXYArea['date'])
     first_day = datetime.datetime(2019, 12, 8) # the time when detected the first case (2019-12-08)
-    DXYArea['Days'] = (DXYArea['updateDate'] - first_day).dt.days
+    DXYArea['Days'] = (DXYArea['date'] - first_day).dt.days
     return DXYArea
 
-def split_train_test_by_date(df: pandas.core.frame.DataFrame, ndays = 3):  ## parameterized split range
+def split_train_test_by_date(df: pandas.core.frame.DataFrame, splicer):
     """
     Separate Train and Test dataset in time series
     """
-    # we use the last 3 days as test data
-    split_date = df['updateDate'].max() - datetime.timedelta(days=ndays)
+    if type(splicer) == float: ## check if splicer is a float
+        ndays = 3 if (df['date'].max() - df['date'].min()).days < 3 else splicer * (df['date'].max() - df['date'].min()).days
+        ndays = np.ceil(ndays)
+    elif type(splicer) == int:
+        ndays = splicer 
+    else:
+        raise Exception('split value should not be greater than length of data')
+
+    split_date = df['date'].max() - datetime.timedelta(days=ndays)
     
     ## Separate Train and Test dataset
-    Train = df[df['updateDate'] < split_date]
-    Test = df[df['updateDate'] >= split_date]
-    print("Train dataset: data before {} \nTest dataset: the last {} days".format(split_date, ndays))
+    Train = df[df['date'] < split_date]
+    Test = df[df['date'] >= split_date]
+    print("Train dataset: data before {} \nTest dataset: the last {} days".format(split_date,ndays))
     
     return Train, Test
 
-def data_processing(df):
+def data_processing(df, splicer, features_to_engineer):
 
-    overall_df = pd.DataFrame(df.groupby(['updateDate']).agg({'confirmed': "sum",
-                                                                'cured': "sum",
-                                                                'dead': 'sum',
-                                                                'Days': 'mean'})).reset_index()
-    Train, Test = split_train_test_by_date(overall_df)
+    overall_df = pd.DataFrame(df.groupby(['date']).agg({'confirmed': "sum",
+                                                        'suspected':'sum',
+                                                        'cured': "sum",
+                                                        'dead': 'sum',
+                                                        'Days': 'mean'})).reset_index()
+    
+    
+    
+    Train, Test = split_train_test_by_date(overall_df, splicer)
+    print(Train)
 
-    X_train = Train['Days']
+    X_train = Train.loc[:,['Days']+[x+'_lag1' for x in features_to_engineer]]
     y_train = Train['confirmed']
-    X_test =  Test['Days']
+    X_test =  Test.loc[:,['Days']+[x+'_lag1' for x in features_to_engineer]]
     y_test = Test['confirmed']
+    
     return X_train, X_test, y_train, y_test
+
+##################
+###      feature engineering
+##################
+
+def feature_engineering(df:pandas.core.frame.DataFrame, features_to_engineer):
+    for feature in features_to_engineer:
+        df[f'{feature}_lag1'] = df[f'{feature}'].shift()
+        df[f'{feature}_lag1'].fillna(0, inplace = True)
+    return df
 
 ##################
 ###           EDA
@@ -64,7 +102,7 @@ def data_processing(df):
 def tsplot_conf_dead_cured(df, title_prefix, figsize=(13,10), fontsize=18, logy=False):
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
-    plot_df = df.groupby('updateDate').agg('sum')
+    plot_df = df.groupby('date').agg('sum')
     plot_df.plot(y=['confirmed'], style='-*', ax=ax1, grid=True, figsize=figsize, logy=logy, color='black', marker='o')
     if logy:
         ax1.set_ylabel("log(confirmed)", color="black", fontsize=14)
@@ -84,14 +122,14 @@ def draw_province_trend(title_prefix: str, df: pandas.core.frame.DataFrame):
     """
     df is the daily dataset from DXY
     """
-    sub_df = df[df['provinceName'] == title_prefix]
+    sub_df = df[df['province'] == title_prefix]
     tsplot_conf_dead_cured(sub_df, title_prefix)
     
 def draw_city_trend(title_prefix: str, df: pandas.core.frame.DataFrame):
     """
     df is the daily dataset from DXY
     """
-    sub_df = df[df['cityName'] == title_prefix]
+    sub_df = df[df['city'] == title_prefix]
     tsplot_conf_dead_cured(sub_df, title_prefix)
 
 
@@ -99,9 +137,7 @@ def draw_city_trend(title_prefix: str, df: pandas.core.frame.DataFrame):
 ##  Modelling
 ###################
 
-### polynomial_regression
-def as_arrary(x):
-    return [np.asarray(x)]
+### general additive model
 
 def draw_fit_plot(degree: int, area: str, X_train, X_test, y_train, y_test, y_train_predicted, y_test_predict, df):
     if len(y_test)>0:
@@ -119,7 +155,7 @@ def draw_fit_plot(degree: int, area: str, X_train, X_test, y_train, y_test, y_tr
     plt.ylabel('Confirmed cases')
     plt.xlabel('2020 Date')
     
-    datemin = df['updateDate'].min()
+    datemin = df['date'].min()
     numdays = len(X_train) + len(X_test)
     labels = list((datemin + datetime.timedelta(days=x)).strftime('%m-%d') for x in range(numdays))
     
@@ -130,68 +166,53 @@ def draw_fit_plot(degree: int, area: str, X_train, X_test, y_train, y_test, y_tr
     plt.show()
     
 
-def create_polynomial_regression_model(degree:int, area:str, df,
-                                       X_train: pandas.core.frame.DataFrame, 
-                                       X_test: pandas.core.frame.DataFrame,
-                                       y_train: pandas.core.frame.DataFrame, 
-                                       y_test: pandas.core.frame.DataFrame,
-                                       draw_plot = False):
-    "Creates a polynomial regression model for the given degree"
+def fit_pygam_model(X_train: pandas.core.frame.DataFrame, 
+                   X_test: pandas.core.frame.DataFrame,
+                   y_train: pandas.core.frame.DataFrame, 
+                   y_test: pandas.core.frame.DataFrame):
+    '''
+    Creates a general additive model LinearGAM (normally distributed errors)
+    with grid search. Returns the best model with given hyperparameters.
+    hyperparameters: n_splines and lam regularization parameter.
+    '''
+    from pygam import LinearGAM
+    gam = LinearGAM().gridsearch(X_train.values, y_train, n_splines=np.arange(3,20), lam = np.logspace(-3, 3, 11))
+    print(gam.summary())
     
-    poly_features = PolynomialFeatures(degree=degree)
-      
-    # transforms the existing features to higher degree features.
-    X_train_array = tuple(map(as_arrary, list(X_train)))
-    X_test_array = tuple(map(as_arrary, list(X_test)))
+    y_train_predicted = gam.predict(X_train)
+    y_test_predicted = np.floor(gam.predict(X_test))
     
-    # Normalize input data
-    X_train_poly = poly_features.fit_transform(X_train_array)
-    
-    # fit the transformed features to Linear Regression
-    poly_model = LinearRegression()
-    poly_model.fit(X_train_poly, y_train)
-      
-    # predicting on training data-set
-    y_train_predicted = poly_model.predict(X_train_poly)
-      
-    # predicting on test data-set
-    y_test_predict = poly_model.predict(poly_features.fit_transform(X_test_array))
-      
-    # evaluating the model on training dataset
     rmse_train = np.sqrt(mean_squared_error(y_train, y_train_predicted))
+    mae_train = mean_absolute_error(y_train, y_train_predicted)
     r2_train = r2_score(y_train, y_train_predicted)
-    print("Degree {}:".format(degree))
     print("RMSE of training set is {}".format(rmse_train))
+    print("MAE of testing set is {}".format(mae_train))
     print("R2 score of training set is {}\n".format(r2_train))
     
     if len(y_test)>0:
-        # evaluating the model on test dataset
-        rmse_test = np.sqrt(mean_squared_error(y_test, y_test_predict))
-        r2_test = r2_score(y_test, y_test_predict)
-        print("RMSE of test set is {}".format(rmse_test))
-        print("R2 score of test set is {}".format(r2_test))
-        
-    print('---------------------------------------\n')
+        rmse_test = np.sqrt(mean_squared_error(y_test, y_test_predicted))
+        mae_test = mean_absolute_error(y_test, y_test_predicted)
+        r2_test = r2_score(y_test, y_test_predicted)
+        print("RMSE of testing set is {}".format(rmse_test))
+        print("MAE of testing set is {}".format(mae_test))
+        print("R2 score of testing set is {}\n".format(r2_test))
     
-    # Draw fit plot
-    if draw_plot == True: 
-        draw_fit_plot(degree, area, X_train, X_test, y_train, y_test, y_train_predicted, y_test_predict, df)
+    '''
+    Visualize the feature significance and confidence intervals
+    '''
+    num_features = len(X_train.columns)
+    fig = plt.figure(figsize=(18,12))
+    fig.subplots_adjust(hspace=0.4)
 
+    cnt = 1
+    p_values = gam.statistics_['p_values']
 
-def forecast_next_4_days(degree: int, area:str, df: pandas.core.frame.DataFrame):
-    """
-    Use all observations to train, based on the 
-    """
-    X_train = df['Days']
-    y_train = df['confirmed']
-    
-    # Create dataset for next 4 days
-    X_test = [df['Days'].max() + 1, df['Days'].max() + 2, df['Days'].max() + 3, df['Days'].max() + 4]
-    y_test = []
-    
-    create_polynomial_regression_model(2, area, df, X_train, X_test, y_train, y_test, draw_plot = True)
-    
-    
-
+    for i in range(num_features):
+        axs = fig.add_subplot(num_features,1,cnt)
+        m = gam.generate_X_grid(term=i)
+        axs.plot(m[:,i], gam.partial_dependence(term=i, X=m)) # this is the actual coefficents
+        axs.plot(m[:,i], gam.partial_dependence(term=i, X=m, width=.95)[1],c='r',ls='--') # this plots the confidence intervals
+        axs.set_title(X_train.columns[i] + ('*' if p_values[cnt]<0.05 else ''))
+        cnt += 1
 
     
